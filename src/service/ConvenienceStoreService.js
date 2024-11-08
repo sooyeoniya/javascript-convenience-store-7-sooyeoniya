adadacaef304ba599ab4309859466a07f66e6008
sooyeoniya <tndus1306403@naver.com>
2024-11-08 16:57:36 +0900

    feat: 프로모션 재고 부족에 대한 안내 메시지 및 수량 갱신 구현

--- src/service/ConvenienceStoreService.js
+++ src/service/ConvenienceStoreService.js
@@ -46,8 +46,11 @@ class ConvenienceStoreService {
     for (const { name, quantity } of this.#productsInfo) {
       if (this.#isAvailablePromotion(name)) {
         const productPromotionStockQuantity = this.#stock.getPromotionStockQuantity(name);
-        if (productPromotionStockQuantity > quantity) await this.#offerAdditionalPromotionItems(name, quantity);
-        else await this.#offerInsufficientPromotionStock(name, quantity);
+        if (productPromotionStockQuantity > quantity) {
+          await this.#offerAdditionalPromotionItems(name, quantity);
+        } else {
+          await this.#offerLackOfPromotionStock(name, quantity, productPromotionStockQuantity);
+        }
       }
     };
   }
@@ -61,14 +64,14 @@ class ConvenienceStoreService {
     };
   }
 
-  // 프로모션 적용 가능 상품에 대해 고객이 해당 수량보다 적게 가져왔는지 확인
+  // 프로모션 적용 가능 상품에 대해 고객이 해당 수량보다 적게 가져왔는지에 대한 안내 메시지 출력 및 수량 갱신
   async #offerAdditionalPromotionItems(productName, productQuantity) {
     const promotionName = this.#stock.getPromotionName(productName);
     const promotionBuyValue = this.#promotion.getPromotionBuyValue(promotionName);
     const promotionGetValue = this.#promotion.getPromotionGetValue(promotionName);
     const promotionBuyPlusGetValue = this.#promotion.getPromotionBuyPlusGetValue(promotionName);
     if (productQuantity % promotionBuyPlusGetValue === promotionBuyValue) {
-      const renewProductQuantity = await this.#renewProductQuantity(productName, productQuantity, promotionGetValue);
+      const renewProductQuantity = await this.#showAdditionalPromotionItemsMessage(productName, productQuantity, promotionGetValue);
       this.#updateProductQuantity(productName, renewProductQuantity);
     }
   }
@@ -79,8 +82,8 @@ class ConvenienceStoreService {
     product.quantity = renewProductQuantity;
   }
 
-  // 안내 메시지 답변에 따른 구매할 수량 재계산
-  async #renewProductQuantity(productName, productQuantity, promotionGetValue) {
+  // 추가 구매 안내 메시지 답변에 따른 구매할 수량 재계산
+  async #showAdditionalPromotionItemsMessage(productName, productQuantity, promotionGetValue) {
     const response = await this.getUserConfirmation(
       `현재 ${productName}은(는) ${promotionGetValue}개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`
     );
@@ -88,12 +91,22 @@ class ConvenienceStoreService {
     if (response.toUpperCase() === 'N') return productQuantity;
   }
 
-  // 프로모션 재고가 부족한지 확인
-  async #offerInsufficientPromotionStock(productName, productQuantity) {
-    // - {수량} = [{프로모션 재고 수량} % 3 or 2] + [{현재상품수량} - {프로모션 재고 수량}]
-    // - 부족한 경우, "현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"
-    // - Y: 아무것도 하지 않음, N: {현재상품수량} - {수량}개, 잘못 입력한 경우: 다시 입력받기
-    // `현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`
+  // 프로모션 재고가 부족한 것에 대한 안내 메시지 출력 및 수량 갱신
+  async #offerLackOfPromotionStock(productName, productQuantity, productPromotionStockQuantity) {
+    const promotionName = this.#stock.getPromotionName(productName);
+    const promotionBuyPlusGetValue = this.#promotion.getPromotionBuyPlusGetValue(promotionName);
+    const nonPromotionalQuantity = (productPromotionStockQuantity % promotionBuyPlusGetValue) + (productQuantity - productPromotionStockQuantity);
+    const renewProductQuantity = await this.#showLackOfPromotionStockMessage(productName, productQuantity, nonPromotionalQuantity);
+    this.#updateProductQuantity(productName, renewProductQuantity);
+  }
+
+  // 재고 부족 안내 메시지 답변에 따른 구매할 수량 재계산
+  async #showLackOfPromotionStockMessage(productName, productQuantity, nonPromotionalQuantity) {
+    const response = await this.getUserConfirmation(
+      `현재 ${productName} ${nonPromotionalQuantity}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`
+    );
+    if (response.toUpperCase() === 'Y') return productQuantity;
+    if (response.toUpperCase() === 'N') return productQuantity - nonPromotionalQuantity;
   }
 
   // 프로모션 적용 가능한 상품인지 아닌지 확인
