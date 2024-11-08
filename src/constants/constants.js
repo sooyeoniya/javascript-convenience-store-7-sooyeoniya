92d5c04642cb3652f2dfc7dea731b98b4ae2b095
sooyeoniya <tndus1306403@naver.com>
2024-11-08 23:33:53 +0900

    feat: 추가 구매 여부 로직 구현
    - 컨트롤러 start 메서드 내부에 추가 구매 반복 로직 구현
    - ProductManagementService, ReceiptService 클래스 인스턴스 생성 내부로 분리
    - 왜냐하면, 반복 구매 시 ProductManagementService, ReceiptService 두 개에 대한 필드 값이 초기화되어야 하기 때문

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,6 +2,7 @@
 export const INPUT_MESSAGES = Object.freeze({
   PRODUCTS_INFO: '\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
   MEMBERSHIP_DISCOUNT: '멤버십 할인을 받으시겠습니까? (Y/N)\n',
+  ADDITIONAL_PURCHASE: '감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)\n',
 });
 
 export const OUTPUT_MESSAGES = Object.freeze({
--- src/controller/ConvenienceStoreController.js
+++ src/controller/ConvenienceStoreController.js
@@ -6,41 +6,52 @@ import OutputView from '../view/OutputView.js';
 import ReceiptService from '../service/ReceiptService.js';
 import ProductManagementService from '../service/ProductManagementService.js';
 import validateProductsToPurchase from '../validations/validateProductsToPurchase.js';
+import getUserConfirmation from '../utils/getUserConfirmation.js';
+import { INPUT_MESSAGES } from '../constants/constants.js';
 
 class ConvenienceStoreController {
   #stock;
   #promotion;
-  #productManagementService;
-  #receiptService;
 
   constructor() {
     this.#stock = new Stock();
     this.#promotion = new Promotion();
-    this.#productManagementService = new ProductManagementService(this.#stock, this.#promotion);
-    this.#receiptService = new ReceiptService(this.#stock, this.#promotion, this.#productManagementService);
   }
 
   async start() {
+    await this.processPurchase();
+
+    let additionalPurchase = await getUserConfirmation(INPUT_MESSAGES.ADDITIONAL_PURCHASE);
+    while (additionalPurchase.toUpperCase() === 'Y') {
+      await this.processPurchase();
+      additionalPurchase = await getUserConfirmation(INPUT_MESSAGES.ADDITIONAL_PURCHASE);
+    }
+  }
+
+  // TODO: 함수 길이 10으로 줄이기
+  async processPurchase() {
     OutputView.printWelcomeGreeting();
     OutputView.printStockInfo(this.#stock.getStockInfo());
 
-    const productsInfo = await this.#validateInputAsync();
-    this.#productManagementService.initProductsInfo(productsInfo);
-    await this.#productManagementService.processProducts();
+    const productManagementService = new ProductManagementService(this.#stock, this.#promotion);
+    const receiptService = new ReceiptService(this.#stock, this.#promotion, productManagementService);
+
+    const productsInfo = await this.#validateInputAsync(productManagementService);
+    productManagementService.initProductsInfo(productsInfo);
+    await productManagementService.processProducts();
     
-    const receiptInfo = await this.#receiptService.processReceipt();
+    const receiptInfo = await receiptService.processReceipt();
     OutputView.printReceipt(receiptInfo);
-    // TODO: 추가 구매 로직 추가
   }
 
-  async #validateInputAsync() {
+  async #validateInputAsync(productManagementService) {
     try {
       const productsToPurchase = await InputView.readProductsInfoAsync();
       const parsedProductsToPurchase = parser.splitEachProduct(productsToPurchase);
-      return validateProductsToPurchase(parsedProductsToPurchase, this.#stock, this.#productManagementService);
+      return validateProductsToPurchase(parsedProductsToPurchase, this.#stock, productManagementService);
     } catch (error) {
       OutputView.printErrorMessage(error.message);
-      return this.#validateInputAsync();
+      return this.#validateInputAsync(productManagementService);
     }
   }
 }
