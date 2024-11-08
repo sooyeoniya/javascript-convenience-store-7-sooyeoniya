862bb62a6d22287eef820e0de770cae9c4275cc2
sooyeoniya <tndus1306403@naver.com>
2024-11-08 16:16:16 +0900

    remove(constants): 사용하지 않는 상수 삭제

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,8 +1,6 @@
 
 export const INPUT_MESSAGES = Object.freeze({
   PRODUCTS_INFO: '\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
-  PROMOTION_BENEFIT: `현재 {상품명}은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n`,
-  PROMOTION_OUT_OF_STOCK: `현재 {상품명} {수량}개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n`,
   MEMBERSHIP_DISCOUNT: '멤버십 할인을 받으시겠습니까? (Y/N)\n',
 });
 
