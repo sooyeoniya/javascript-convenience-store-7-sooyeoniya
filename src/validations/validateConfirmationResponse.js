69ce0032f53f094dde6a2d3cef15e2143686f66c
sooyeoniya <tndus1306403@naver.com>
2024-11-10 21:29:33 +0900

    fix: 안내 메시지에 대한 입력 값 Y/N 대문자만 허용하도록 수정

--- src/validations/validateConfirmationResponse.js
+++ src/validations/validateConfirmationResponse.js
@@ -1,11 +1,11 @@
 import { ERROR_MESSAGES, CONFIRMATION_RESPONSES } from '../constants/constants.js';
 
 const validateConfirmationResponse = (userInput) => {
-  if (![CONFIRMATION_RESPONSES.YES, CONFIRMATION_RESPONSES.NO].includes(userInput.toUpperCase())) {
+  if (![CONFIRMATION_RESPONSES.YES, CONFIRMATION_RESPONSES.NO].includes(userInput)) {
     throw new Error(ERROR_MESSAGES.OTHER_ERRORS);
   }
 
-  return userInput.toUpperCase();
+  return userInput;
 }
 
 export default validateConfirmationResponse;
